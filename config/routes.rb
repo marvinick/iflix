39f0f8359e11acd59c88ae03929d27a132e45ea6
marvinick <marviluw@gmail.com>
2013-11-26 19:32:05 -0600

    show video

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Myflix::Application.routes.draw do
   get 'ui(/:action)', controller: 'ui'
   get 'home', to: "videos#index"
+  get 'show', to: "videos#show"
+
+  resources :videos, only: [:show]
 end
