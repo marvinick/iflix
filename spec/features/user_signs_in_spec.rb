e5c828289cf70c253e0e2c48bd31c5787b7e96cd
marvinick <marviluw@gmail.com>
2013-12-07 15:43:02 -0600

    more complex feature specs

--- spec/features/user_signs_in_spec.rb
+++ spec/features/user_signs_in_spec.rb
@@ -4,10 +4,7 @@ feature 'user signs in' do
 
   scenario "with valid user email and password" do
     marv = Fabricate(:user)
-    visit sign_in_path
-    fill_in "Email Address", with: marv.email
-    fill_in "Password", with: marv.password
-    click_button "Sign in"
+    sign_in(marv)
     page.should have_content marv.full_name
   end
 end
