fb08811b7719f36238e5027eb46b9772a7203985
marvinick <marviluw@gmail.com>
2013-12-15 11:01:28 -0600

    admin adds video

--- app/controllers/admin/videos_controller.rb
+++ app/controllers/admin/videos_controller.rb
@@ -6,7 +6,22 @@ class Admin::VideosController < ApplicationController
     @video = Video.new
   end
 
+  def create
+    @video = Video.create(video_params)
+    if @video.save
+      flash[:success] = "You have added a video '#{@video.title}'"
+      redirect_to new_admin_video_path
+    else
+      flash[:error] = "Something's wrong. Please check your inputs"
+      render :new
+    end
+  end
+
   private
+
+  def video_params
+    params.require(:video).permit(:title, :category_id, :description)
+  end
 
   def require_admin
     if !current_user.admin?
